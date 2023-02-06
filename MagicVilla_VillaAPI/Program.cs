789b7a9113b0902beca771b21b6d1e60e795b6b6
pinaki swain <pinakis@mindfiresolutions.com>
2023-02-06 19:13:42 +0530

    How to use Serilog tool to log

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,7 +1,11 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+Log.Logger = new LoggerConfiguration().MinimumLevel.Information()
+    .WriteTo.File("log/VillLog.txt").CreateLogger();
+builder.Host.UseSerilog();
 builder.Services.AddControllers(options=>options.ReturnHttpNotAcceptable=true).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

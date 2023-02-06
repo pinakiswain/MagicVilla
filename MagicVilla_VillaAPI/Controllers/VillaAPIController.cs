5b7ea953d9cbd9acd4521e50bce6ce29e56ae145
pinaki swain <pinakis@mindfiresolutions.com>
2023-02-06 18:56:38 +0530

    Add default logging to show in cmd

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -11,9 +11,16 @@ namespace MagicVilla_VillaAPI.Controllers
     [ApiController]
     public class VillaAPIController: ControllerBase
     {
+        private readonly ILogger<VillaAPIController> _logger;
+
+        public VillaAPIController(ILogger<VillaAPIController> logger)
+        {
+            _logger = logger;
+        }
         [HttpGet(Name ="GetallVilla")]
         public ActionResult <IEnumerable<VillaDto>> GetVillas()
         {
+            _logger.LogInformation("Get all villas");
             return Ok(VillaStore.villas);//200
 
         }
